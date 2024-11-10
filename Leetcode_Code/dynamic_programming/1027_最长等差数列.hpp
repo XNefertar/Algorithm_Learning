0ee553e9538f70e68101160a7dcbd3cdf7513d89
X_Nefertar <xnefertar@gmail.com>
2024-11-10 22:17:34 +0800

    Update

---
+++ Leetcode_Code/dynamic_programming/1027_最长等差数列.hpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestArithSeqLength(vector<int>& nums) {
+        int n = nums.size(), Max = 2;
+        unordered_map<int, int> hash;
+        vector<vector<int>> dp(n, vector<int>(n, 2));
+        hash[nums[0]] = 0;
+        for(int i = 1; i < n; ++i){
+            for(int j = i + 1; j < n; ++j){
+                int target = 2 * nums[i] - nums[j];
+                if(hash.count(target)){
+                    dp[i][j] = dp[hash[target]][i] + 1;
+                }
+                Max = max(Max, dp[i][j]);
+            }
+            hash[nums[i]] = i;
+        }
+        return Max;
+    }
+};

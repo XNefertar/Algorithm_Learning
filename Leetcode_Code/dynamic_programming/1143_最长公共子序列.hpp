5ca6f84e01e4cc24b2d81697c4aeceebfad56898
X_Nefertar <xnefertar@gmail.com>
2024-11-15 00:02:31 +0800

    Update

---
+++ Leetcode_Code/dynamic_programming/1143_最长公共子序列.hpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n1 = text1.size(), n2 = text2.size();
+        // int maxn = n1 > n2 ? n1 : n2;
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        for(int i = 1; i <= n1; ++i){
+            for(int j = 1; j <= n2; ++j){
+                if(text1[i - 1] == text2[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else{
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+};

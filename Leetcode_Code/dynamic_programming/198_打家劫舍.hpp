3ac17bdac440fec7c39fdcb4d41c263f2e76475f
X_Nefertar <xnefertar@gmail.com>
2024-12-07 10:00:00 +0800

    Update_Annotations_198

--- Leetcode_Code/dynamic_programming/198_打家劫舍.hpp
+++ Leetcode_Code/dynamic_programming/198_打家劫舍.hpp
@@ -1,13 +1,18 @@
-
+// Explanation
+// 1. Why not use a one-dimensional dp table?
+// You can't tell the status of the previous house through a 1D DP table - whether it was stolen or not
+// Therefore, a two-dimensional dp table needs to be used to represent the current state
+// Here I have made another optimization, using two 1D DP tables instead of 2D DP tables, which is easier to understand
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        // f[i] 表示第i家必不抢
-        // g[i] 表示第i家必抢
+        // The first one-dimensional dp table f[i] indicates that the ith family must not steal
+        // The second one-dimensional dp table g[i] indicates that the ith family will definitely steal
         vector<int> f(n + 1), g(n + 1);
         g[0] = nums[0];
         for(int i = 1; i < n; ++i){
+            // So there is a simple logical relationship between the two, from which the state transition equation can be derived
             g[i] = f[i - 1] + nums[i];
             f[i] = max(f[i - 1], g[i - 1]);
         }
